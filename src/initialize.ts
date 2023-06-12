0a9f008a83f9d708ebde2789d2e5aa7fa0f6b4e3
Nikola Tesla <tesla@buffer.finance>
2023-06-13 03:19:40 +0530

    Fix

--- src/initialize.ts
+++ src/initialize.ts
@@ -115,6 +115,7 @@ export function _loadOrCreateTradingStatEntity(
     entity.profitCumulativeUSDC = ZERO;
     entity.lossCumulativeUSDC = ZERO;
     entity.profitARB = ZERO;
+    entity.profitBFR = ZERO;
     entity.lossARB = ZERO;
     entity.lossBFR = ZERO;
     entity.profitCumulativeARB = ZERO;

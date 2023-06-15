14ec407226962495087e1310888df581bd005fd7
Nikola Tesla <tesla@buffer.finance>
2023-06-15 17:34:43 +0530

    Added bfr tracking

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,17 +1,23 @@
 import { Transfer } from "../generated/BFR/BFR";
 import { BurnedBFR } from "../generated/schema";
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { _getDayId } from "./helpers";
 
-export const ZERO = BigInt.fromI32(0);
+const ZERO = BigInt.fromI32(0);
+const BURN_ADDRESS = "0x000000000000000000000000000000000000dEaD";
 
 export function handleTransfer(event: Transfer): void {
-  let id = _
-  let entity = _loadOrCreateBurnedBFR("daily", id);  
+  let timestamp = event.block.timestamp;
+  if (event.params.to == Address.fromString(BURN_ADDRESS)) {
+    let id = _getDayId(timestamp);
+    let entity = _loadOrCreateBurnedBFR("daily", id);
+    entity.amount = entity.amount.plus(event.params.value);
+    entity.timestamp = timestamp;
+    entity.save();
+  }
 }
 
-
-
-function _loadOrCreateBurnedBFR(period: string, id: string, ): BurnedBFR {
+function _loadOrCreateBurnedBFR(period: string, id: string): BurnedBFR {
   let entity = BurnedBFR.load(id);
   if (!entity) {
     entity = new BurnedBFR(id);

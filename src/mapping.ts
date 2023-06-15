8617d7382f1c2ef53dc3c2326c3968b0a870cac1
Nikola Tesla <tesla@buffer.finance>
2023-06-15 14:19:08 +0530

    WIP

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,70 +1,19 @@
-import {
-  Create,
-  Expire,
-  Exercise,
-  UpdateReferral,
-  Pause,
-  CreateOptionsContract,
-} from "../generated/BufferBinaryOptions/BufferBinaryOptions";
-import {
-  UpdateMaxPeriod,
-  UpdateMinFee,
-  UpdateMinPeriod,
-} from "../generated/BufferConfigUpdates/BufferConfig";
-import {
-  _handleCreate,
-  _handleExpire,
-  _handleExercise,
-  _handleUpdateReferral,
-  _handlePause,
-} from "./optionContractHandlers";
-import { _handleOpenTrade } from "./routerContractHandlers";
-import {
-  _handleCreateOptionsContract,
-  _handleUpdateMaxPeriod,
-  _handleUpdateMinFee,
-  _handleUpdateMinPeriod,
-} from "./configContractHandlers";
-import { OpenTrade } from "../generated/BufferRouter/BufferRouter";
+import { Transfer } from "../generated/BFR/BFR";
 
-export function handleOpenTrade(event: OpenTrade): void {
-  _handleOpenTrade(event);
+export function handleTransfer(event: Transfer): void {
+  _handleTransfer(event);
 }
 
-export function handleCreate(event: Create): void {
-  _handleCreate(event);
-}
 
-export function handleExercise(event: Exercise): void {
-  _handleExercise(event);
-}
 
-export function handleExpire(event: Expire): void {
-  _handleExpire(event);
-}
-
-export function handleUpdateReferral(event: UpdateReferral): void {
-  _handleUpdateReferral(event);
-}
-
-export function handlePause(event: Pause): void {
-  _handlePause(event);
-}
-
-export function handleCreateOptionsContract(
-  event: CreateOptionsContract
-): void {
-  _handleCreateOptionsContract(event);
-}
-
-export function handleUpdateMinFee(event: UpdateMinFee): void {
-  _handleUpdateMinFee(event);
-}
-
-export function handleUpdateMaxPeriod(event: UpdateMaxPeriod): void {
-  _handleUpdateMaxPeriod(event);
-}
-
-export function handleUpdateMinPeriod(event: UpdateMinPeriod): void {
-  _handleUpdateMinPeriod(event);
+function loadOrCreateBFRHolderData(timestamp: BigInt, period: string, id: string): BFRHolderData {
+  let entity = BFRHolderData.load(id);
+  if (!entity) {
+    entity = new BFRHolderData(id);
+    entity.holders = 0;
+    entity.period = period;
+    entity.timestamp = timestamp;
+    entity.save();
+  }
+  return entity;
 }

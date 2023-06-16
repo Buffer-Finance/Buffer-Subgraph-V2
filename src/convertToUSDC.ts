09d13f48aafddb284591219251b5f8cd59f8510a
Nikola Tesla <tesla@buffer.finance>
2023-06-16 12:33:28 +0530

    Fix

--- src/convertToUSDC.ts
+++ src/convertToUSDC.ts
@@ -3,7 +3,9 @@ import { UniswapPool } from "../generated/UniswapPool/UniswapPool";
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 
 export function convertARBToUSDC(amount: BigInt): BigInt {
-  let uniswapPool = UniswapPool.bind(Address.fromString(ARB_UNISWAP_POOL_CONTRACT));
+  let uniswapPool = UniswapPool.bind(
+    Address.fromString(ARB_UNISWAP_POOL_CONTRACT)
+  );
   let slot0_data = uniswapPool.slot0();
   let arbToUSDC = slot0_data.value0
     .pow(2)
@@ -13,11 +15,13 @@ export function convertARBToUSDC(amount: BigInt): BigInt {
 }
 
 export function convertBFRToUSDC(amount: BigInt): BigInt {
-  let uniswapPool = UniswapPool.bind(Address.fromString(BFR_UNISWAP_POOL_CONTRACT));
+  let uniswapPool = UniswapPool.bind(
+    Address.fromString(BFR_UNISWAP_POOL_CONTRACT)
+  );
   let slot0_data = uniswapPool.slot0();
   let bfrToUSDC = slot0_data.value0
     .pow(2)
     .times(amount)
     .div(BigInt.fromI32(2).pow(192));
-  return bfrToUSDC;
+  return bfrToUSDC.div(BigInt.fromI32(2));
 }

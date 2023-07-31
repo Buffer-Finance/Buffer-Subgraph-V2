2528e9eb9f41db3211a7a2a04a9cd8ee2c81a73a
CurieBuffer <curie@buffer.finance>
2023-07-31 18:11:46 +0530

    contracts changed

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
 export enum State {
-  active = 1,
-  exercised = 2,
-  expired = 3,
-  queued = 4,
-  cancelled = 5,
-  opened = 6,
+    active = 1,
+    exercised = 2,
+    expired = 3,
+    queued = 4,
+    cancelled = 5,
+    opened = 6
 }
 
-export const RouterAddress = "0x92B81a2E32a01D5d22c5e822801aa32aA144bfd6";
-export const USDC_ADDRESS = "0x50E345c95a3c1E5085AE886FF4AF05Efa2403c90";
+export const RouterAddress = "0x26f3585B2746B0038b5E0cda69F40d7f9D8d8538";
+export const USDC_ADDRESS = "0x943902252597Ca568a6fB076aceCEdcf7eb903C6";
 export const ARBITRUM_SOLANA_ADDRESS =
-  "0x0000000000000000000000000000000000000000";
+    "0x0000000000000000000000000000000000000000";
 export const ARB_TOKEN_ADDRESS = "0x0000000000000000000000000000000000000000";
-export const USDC_POOL_CONTRACT = "0xA970D4c3965FFb771bB8F9E52e33417E5907a1e7";
+export const USDC_POOL_CONTRACT = "0x6EfAbB45b781B62979600444775113516220d992";
 export const ARB_POOL_CONTRACT = "0x0000000000000000000000000000000000000000";

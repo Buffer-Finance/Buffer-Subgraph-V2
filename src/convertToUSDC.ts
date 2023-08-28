9fcf6f21361d7b54d39d3a8a416c343fd39cbbe6
Faraday26 <faraday@buffer.finance>
2023-08-28 22:27:19 +0530

    update convert functions

--- src/convertToUSDC.ts
+++ src/convertToUSDC.ts
@@ -1,9 +1,27 @@
+import { ARB_UNISWAP_POOL_CONTRACT, BFR_UNISWAP_POOL_CONTRACT } from "./config";
+import { UniswapPool } from "../generated/BufferBinaryOptions/UniswapPool";
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 
 export function convertARBToUSDC(amount: BigInt): BigInt {
-  return amount.times(BigInt.fromI32(12)).div(BigInt.fromI64(100000000000000));
+  let uniswapPool = UniswapPool.bind(
+    Address.fromString(ARB_UNISWAP_POOL_CONTRACT)
+  );
+  let slot0_data = uniswapPool.slot0();
+  let arbToUSDC = slot0_data.value0
+    .pow(2)
+    .times(amount)
+    .div(BigInt.fromI32(2).pow(192));
+  return arbToUSDC;
 }
 
 export function convertBFRToUSDC(amount: BigInt): BigInt {
-  return amount.times(BigInt.fromI32(12)).div(BigInt.fromI64(10000000000000));
+  let uniswapPool = UniswapPool.bind(
+    Address.fromString(BFR_UNISWAP_POOL_CONTRACT)
+  );
+  let slot0_data = uniswapPool.slot0();
+  let bfrToUSDC = slot0_data.value0
+    .pow(2)
+    .times(amount)
+    .div(BigInt.fromI32(2).pow(192));
+  return bfrToUSDC.div(BigInt.fromI32(2));
 }

92573371a9d7293e8eca91bd2827b1ad8f38f7f0
Faraday <faraday@buffer.finance>
2023-12-05 18:04:17 +0530

    update contracts

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export enum State {
   opened = 6,
 }
 
-export const RouterAddress = "0x6eF3641353386eA5823cCf114bE813463fd1A5a4";
+export const RouterAddress = "0x0Fc0f8E3F458C75A3B80683dD66A1458E90F0c66";
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";

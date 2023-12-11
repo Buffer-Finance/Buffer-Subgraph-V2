52bf103fc145c1c323176d05aacb59c7bec0fc29
Faraday <faraday@buffer.finance>
2023-12-11 13:04:53 +0530

    add sepolia contracts

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@ export enum State {
   opened = 6,
 }
 
-export const RouterAddress = "0x6e57AAb1659f7f787F893adD176bA15F0e2Be067";
+export const RouterAddress = "0x9bE7abE78A8fc78eDc763fDB864B50c6e98ACB97";
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 export const ONE = BigInt.fromI32(1);

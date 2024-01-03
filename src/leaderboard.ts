9178ab3f1e7509bbac864b403f1d0c0aa11a5505
Faraday <faraday@buffer.finance>
2024-01-03 12:36:26 +0530

    add all time leaderboard

--- src/leaderboard.ts
+++ src/leaderboard.ts
@@ -56,6 +56,22 @@ export function updateLeaderboards(
     isBFR,
     bfrVolume
   );
+  _updateAllTimeLeaderboard(
+    totalFee,
+    timestamp,
+    user,
+    isExercised,
+    arbVolume,
+    isUSDC,
+    usdcVolume,
+    isARB,
+    netPnL,
+    arbNetPnL,
+    usdcNetPnL,
+    bfrNetPnL,
+    isBFR,
+    bfrVolume
+  );
 }
 
 //To calculate Reward Pool for leaderboards
@@ -178,6 +194,76 @@ function _updateWeeklyLeaderboard(
     _getLeaderboardWeekId(timestamp),
     user
   );
+  entity.volume = entity.volume.plus(totalFee);
+  entity.totalTrades += 1;
+  entity.netPnL = isExercised
+    ? entity.netPnL.plus(netPnL)
+    : entity.netPnL.minus(netPnL);
+  if (isExercised) {
+    entity.tradesWon += 1;
+  }
+  entity.winRate = (entity.tradesWon * 100000) / entity.totalTrades;
+
+  entity.arbVolume = entity.arbVolume.plus(arbVolume);
+
+  entity.bfrVolume = entity.bfrVolume.plus(bfrVolume);
+
+  entity.arbNetPnL = isExercised
+    ? entity.arbNetPnL.plus(arbNetPnL)
+    : entity.arbNetPnL.minus(arbNetPnL);
+
+  entity.bfrNetPnL = isExercised
+    ? entity.bfrNetPnL.plus(bfrNetPnL)
+    : entity.bfrNetPnL.minus(bfrNetPnL);
+
+  let arbTotalTrades = isARB ? 1 : 0;
+  let bfrTotalTrades = isBFR ? 1 : 0;
+
+  entity.arbTotalTrades += arbTotalTrades;
+  entity.bfrTotalTrades += bfrTotalTrades;
+
+  entity.arbTradesWon += isExercised ? arbTotalTrades : 0;
+  entity.bfrTradesWon += isExercised ? bfrTotalTrades : 0;
+
+  if (entity.arbTotalTrades > 0) {
+    entity.arbWinRate = (entity.arbTradesWon * 100000) / entity.arbTotalTrades;
+  }
+
+  if (entity.bfrTotalTrades > 0) {
+    entity.bfrWinRate = (entity.bfrTradesWon * 100000) / entity.bfrTotalTrades;
+  }
+
+  entity.usdcVolume = entity.usdcVolume.plus(usdcVolume);
+  entity.usdcNetPnL = isExercised
+    ? entity.usdcNetPnL.plus(usdcNetPnL)
+    : entity.usdcNetPnL.minus(usdcNetPnL);
+  let usdcTotalTrades = isUSDC ? 1 : 0;
+  entity.usdcTotalTrades += usdcTotalTrades;
+  entity.usdcTradesWon += isExercised ? usdcTotalTrades : 0;
+  if (entity.usdcTotalTrades > 0) {
+    entity.usdcWinRate =
+      (entity.usdcTradesWon * 100000) / entity.usdcTotalTrades;
+  }
+  entity.save();
+}
+
+function _updateAllTimeLeaderboard(
+  totalFee: BigInt,
+  timestamp: BigInt,
+  user: string,
+  isExercised: boolean,
+  arbVolume: BigInt,
+  isUSDC: boolean,
+  usdcVolume: BigInt,
+  isARB: boolean,
+  netPnL: BigInt,
+  arbNetPnL: BigInt,
+  usdcNetPnL: BigInt,
+  bfrNetPnL: BigInt,
+  isBFR: boolean,
+  bfrVolume: BigInt
+): void {
+  let entity = _loadOrCreateLeaderboardEntity("allTime", user);
   entity.volume = entity.volume.plus(totalFee);
   entity.totalTrades += 1;
   entity.netPnL = isExercised

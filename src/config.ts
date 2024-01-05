df5fd7726f210453d7dd81e4195ee4a59012c104
Faraday <faraday@buffer.finance>
2024-01-05 12:20:30 +0530

    change contracts

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,6 @@ export enum State {
   opened = 6,
 }
 
-export const RouterAddress = "0x636D2D526073bF03024D716DbE781a37C840Bce0";
+export const RouterAddress = "0x912B29A5efeA11d7994970799E82C3A7FB54a434";
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 export const ONE = BigInt.fromI32(1);

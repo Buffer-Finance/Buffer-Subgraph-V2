69cd5b9460af69ac8960a337a2b584980d8c8098
Faraday <faraday@buffer.finance>
2024-03-14 09:00:53 +0530

    wip

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,14 +1,91 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { PoolStat } from "../generated/schema";
+import { _getDayId } from "./helpers";
 let ZERO = BigInt.fromI32(0);
 
-export function _loadOrCreatePoolStat(id: string, period: string): PoolStat {
-  let poolStat = PoolStat.load(id);
+export function _updateOrCreatePoolStatForProvideWithdraw(
+  timestamp: BigInt,
+  amount: BigInt,
+  blp_amount: BigInt,
+  isProvide: boolean
+): void {
+  const id = _getDayId(timestamp);
+  let poolStat = PoolStat.load(id.toString());
   if (poolStat == null) {
-    poolStat = new PoolStat(id);
-    poolStat.amount = ZERO;
-    poolStat.period = period;
-    poolStat.rate = ZERO;
+    const pastDayPoolStats = PoolStat.load((id - 1).toString());
+    if (pastDayPoolStats != null) {
+      poolStat = new PoolStat(id.toString());
+      const newAmount = isProvide
+        ? pastDayPoolStats.amount.plus(amount)
+        : pastDayPoolStats.amount.minus(amount);
+      const newBlpAmount = isProvide
+        ? pastDayPoolStats.blp_amount.plus(blp_amount)
+        : pastDayPoolStats.blp_amount.minus(blp_amount);
+      poolStat.amount = newAmount;
+      poolStat.rate = pastDayPoolStats.rate;
+      poolStat.blp_amount = newBlpAmount;
+      poolStat.timestamp = timestamp;
+      poolStat.save();
+    } else {
+      const poolStat = new PoolStat(id.toString());
+      poolStat.amount = isProvide ? amount : ZERO.minus(amount);
+      poolStat.rate = amount.div(blp_amount);
+      poolStat.blp_amount = isProvide ? blp_amount : ZERO.minus(blp_amount);
+      poolStat.timestamp = timestamp;
+      poolStat.save();
+    }
+  } else {
+    poolStat = new PoolStat(id.toString());
+    const newAmount = isProvide
+      ? poolStat.amount.plus(amount)
+      : poolStat.amount.minus(amount);
+    const newBlpAmount = isProvide
+      ? poolStat.blp_amount.plus(blp_amount)
+      : poolStat.blp_amount.minus(blp_amount);
+    poolStat.amount = newAmount;
+    poolStat.rate = newBlpAmount;
+    poolStat.blp_amount = poolStat.blp_amount.plus(blp_amount);
+    poolStat.timestamp = timestamp;
+    poolStat.save();
   }
-  return poolStat as PoolStat;
+}
+
+export function _updateOrCreatePoolStatForProfitLoss(
+  timestamp: BigInt,
+  amount: BigInt,
+  isProfit: boolean
+): void {
+  const id = _getDayId(timestamp);
+  let poolStat = PoolStat.load(id.toString());
+  if (poolStat == null) {
+    const pastDayPoolStats = PoolStat.load((id - 1).toString());
+    if (pastDayPoolStats != null) {
+      poolStat = new PoolStat(id.toString());
+      const newAmount = isProfit
+        ? pastDayPoolStats.amount.plus(amount)
+        : pastDayPoolStats.amount.minus(amount);
+      poolStat.amount = newAmount;
+      poolStat.rate = newAmount.div(pastDayPoolStats.blp_amount);
+      poolStat.blp_amount = pastDayPoolStats.blp_amount;
+      poolStat.timestamp = timestamp;
+      poolStat.save();
+    } else {
+      const poolStat = new PoolStat(id.toString());
+      poolStat.amount = isProfit ? amount : ZERO.minus(amount);
+      poolStat.rate = ZERO;
+      poolStat.blp_amount = ZERO;
+      poolStat.timestamp = timestamp;
+      poolStat.save();
+    }
+  } else {
+    poolStat = new PoolStat(id.toString());
+    let newAmount = isProfit
+      ? poolStat.amount.plus(amount)
+      : poolStat.amount.minus(amount);
+    poolStat.amount = newAmount;
+    poolStat.rate = newAmount.div(poolStat.blp_amount);
+    poolStat.blp_amount = poolStat.blp_amount;
+    poolStat.timestamp = timestamp;
+    poolStat.save();
+  }
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,374 +1,106 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
-  BinaryPool,
   Loss,
   Profit,
   Provide,
   Withdraw,
 } from "../generated/BinaryPool/BinaryPool";
-import { USDC } from "../generated/USDC/USDC";
-import { USDC_ADDRESS } from "./config";
-import { _getDayId } from "./helpers";
-import { _loadOrCreatePoolStat } from "./initialize";
-
-// export function handleInitiateTrade(event: InitiateTrade): void {
-//     let routerContract = BufferRouter.bind(event.address);
-//     let queueID = event.params.queueId;
-//     let queuedTradeData = routerContract.queuedTrades(queueID);
-//     let contractAddress = queuedTradeData.value6;
-//     _loadOrCreateOptionContractEntity(contractAddress);
-//     logUser(event.block.timestamp, event.params.account);
-//     let queuedOptionData = _loadOrCreateQueuedOptionEntity(
-//         queueID,
-//         contractAddress
-//     );
-//     queuedOptionData.user = event.params.account;
-//     queuedOptionData.state = State.queued;
-//     queuedOptionData.strike = queuedTradeData.value7;
-//     queuedOptionData.totalFee = queuedTradeData.value3;
-//     queuedOptionData.slippage = queuedTradeData.value8;
-//     queuedOptionData.isAbove = queuedTradeData.value5 ? true : false;
-//     queuedOptionData.queuedTimestamp = event.block.timestamp;
-//     queuedOptionData.save();
-// }
-
-// export function handleOpenTrade(event: OpenTrade): void {
-//     let routerContract = BufferRouter.bind(event.address);
-//     let queueID = event.params.queueId;
-//     let contractAddress = routerContract.queuedTrades(queueID).value6;
-//     let userQueuedData = _loadOrCreateQueuedOptionEntity(
-//         queueID,
-//         contractAddress
-//     );
-//     userQueuedData.lag = event.block.timestamp.minus(
-//         userQueuedData.queuedTimestamp
-//     );
-//     userQueuedData.processTime = event.block.timestamp;
-//     userQueuedData.state = State.opened;
-//     userQueuedData.save();
-//     let userOptionData = _loadOrCreateOptionDataEntity(
-//         event.params.optionId,
-//         contractAddress
-//     );
-//     userOptionData.queueID = queueID;
-//     userOptionData.queuedTimestamp = userQueuedData.queuedTimestamp;
-//     userOptionData.lag = event.block.timestamp.minus(
-//         userQueuedData.queuedTimestamp
-//     );
-//     userOptionData.save();
-// }
-
-// export function handleCreate(event: Create): void {
-//     _handleCreate(event);
-// }
-
-// export function handleCancelTrade(event: CancelTrade): void {
-//     let queueID = event.params.queueId;
-//     let routerContract = BufferRouter.bind(event.address);
-//     let contractAddress = routerContract.queuedTrades(queueID).value6;
-//     let userQueuedData = _loadOrCreateQueuedOptionEntity(
-//         queueID,
-//         contractAddress
-//     );
-//     userQueuedData.state = State.cancelled;
-//     userQueuedData.reason = event.params.reason;
-//     userQueuedData.save();
-// }
-
-// export function handleExercise(event: Exercise): void {
-//     let routerContract = BufferRouter.bind(Address.fromString(RouterAddress));
-//     if ((routerContract.contractRegistry(event.address) == true) || (event.address == Address.fromString(ARBITRUM_SOLANA_ADDRESS))) {
-//         let userOptionData = _loadOrCreateOptionDataEntity(
-//             event.params.id,
-//             event.address
-//         );
-//         userOptionData.state = State.exercised;
-//         userOptionData.payout = event.params.profit;
-//         userOptionData.expirationPrice = event.params.priceAtExpiration;
-//         userOptionData.save();
-//         let optionContractInstance = BufferBinaryOptions.bind(event.address);
-//         let optionContractData = _loadOrCreateOptionContractEntity(
-//             event.address
-//         );
-//         optionContractData.currentUtilization = calculateCurrentUtilization(
-//             optionContractInstance
-//         );
-//         let timestamp = userOptionData.creationTime;
-//         if (userOptionData.isAbove) {
-//             optionContractData.openUp = optionContractData.openUp.minus(userOptionData.totalFee);
-//           } else {
-//             optionContractData.openDown = optionContractData.openDown.minus(userOptionData.totalFee);
-//         }
-//         optionContractData.openInterest = optionContractData.openInterest.minus(userOptionData.totalFee);
-//         optionContractData.save();
-//         if (optionContractInstance.tokenX() == Address.fromString(USDC_ADDRESS)) {
-//             updateOpenInterest(
-//                 timestamp,
-//                 false,
-//                 userOptionData.isAbove,
-//                 userOptionData.totalFee
-//             );
-//             let profit = userOptionData.totalFee.minus(userOptionData.settlementFee);
-//             storePnl(timestamp, profit, true);
-//             storePnlPerContract(timestamp, profit, true, event.address);
-
-//             // Leaderboard
-//             let leaderboardEntity = _loadOrCreateLeaderboardEntity(
-//                 _getDayId(timestamp),
-//                 userOptionData.user
-//             );
-//             leaderboardEntity.volume = leaderboardEntity.volume.plus(
-//                 userOptionData.totalFee
-//             );
-//             leaderboardEntity.totalTrades = leaderboardEntity.totalTrades + 1;
-//             leaderboardEntity.netPnL = leaderboardEntity.netPnL.plus(
-//                 event.params.profit.minus(userOptionData.totalFee)
-//             );
-//             leaderboardEntity.save();
-
-//             // Weekly Leaderboard
-//             let WeeklyLeaderboardEntity = _loadOrCreateWeeklyLeaderboardEntity(
-//                 _getWeekId(timestamp),
-//                 userOptionData.user
-//             );
-//             WeeklyLeaderboardEntity.volume = WeeklyLeaderboardEntity.volume.plus(
-//                 userOptionData.totalFee
-//             );
-//             WeeklyLeaderboardEntity.totalTrades = WeeklyLeaderboardEntity.totalTrades + 1;
-//             WeeklyLeaderboardEntity.netPnL = WeeklyLeaderboardEntity.netPnL.plus(
-//                 event.params.profit.minus(userOptionData.totalFee)
-//             );
-//             WeeklyLeaderboardEntity.tradesWon = WeeklyLeaderboardEntity.tradesWon + 1;
-//             WeeklyLeaderboardEntity.winRate = (WeeklyLeaderboardEntity.tradesWon * 100000) / WeeklyLeaderboardEntity.totalTrades;
-//             WeeklyLeaderboardEntity.save();
-//         }
-//     }
-// }
-
-// export function handleExpire(event: Expire): void {
-//     let routerContract = BufferRouter.bind(Address.fromString(RouterAddress));
-//     if ((routerContract.contractRegistry(event.address) == true) || (event.address == Address.fromString(ARBITRUM_SOLANA_ADDRESS))) {
-//         let referrenceID = `${event.params.id}${event.address}`;
-//         let userOptionData = UserOptionData.load(referrenceID);
-//         if (userOptionData != null) {
-//             userOptionData.state = State.expired;
-//             userOptionData.expirationPrice = event.params.priceAtExpiration;
-//             userOptionData.save();
-//             let timestamp = userOptionData.creationTime;
-//             let optionContractInstance = BufferBinaryOptions.bind(
-//                 event.address
-//             );
-//             let optionContractData = _loadOrCreateOptionContractEntity(
-//                 event.address
-//             );
-//             optionContractData.currentUtilization = calculateCurrentUtilization(
-//                 optionContractInstance
-//             );
-//             if (userOptionData.isAbove) {
-//               optionContractData.openUp = optionContractData.openUp.minus(userOptionData.totalFee);
-//             } else {
-//               optionContractData.openDown = optionContractData.openDown.minus(userOptionData.totalFee);
-//             }
-//             optionContractData.openInterest = optionContractData.openInterest.minus(userOptionData.totalFee);
-//             optionContractData.save();
-//             if (optionContractInstance.tokenX() == Address.fromString(USDC_ADDRESS)) {
-//                 updateOpenInterest(
-//                     timestamp,
-//                     false,
-//                     userOptionData.isAbove,
-//                     userOptionData.totalFee
-//                 );
-//                 storePnl(
-//                     timestamp,
-//                     userOptionData.totalFee.minus(userOptionData.settlementFee),
-//                     false
-//                 );
-//                 storePnlPerContract(
-//                     timestamp,
-//                     userOptionData.totalFee.minus(userOptionData.settlementFee),
-//                     false,
-//                     event.address
-//                 );
-
-//                 // Leaderboard
-//                 let leaderboardEntity = _loadOrCreateLeaderboardEntity(
-//                     _getDayId(timestamp),
-//                     userOptionData.user
-//                 );
-//                 leaderboardEntity.volume = leaderboardEntity.volume.plus(
-//                     userOptionData.totalFee
-//                 );
-//                 leaderboardEntity.totalTrades =
-//                     leaderboardEntity.totalTrades + 1;
-//                 leaderboardEntity.netPnL = leaderboardEntity.netPnL.minus(
-//                     userOptionData.totalFee
-//                 );
-//                 leaderboardEntity.save();
-
-//                 // Weekly Leaderboard
-//                 let WeeklyLeaderboardEntity = _loadOrCreateWeeklyLeaderboardEntity(
-//                     _getWeekId(timestamp),
-//                     userOptionData.user
-//                 );
-//                 WeeklyLeaderboardEntity.volume = WeeklyLeaderboardEntity.volume.plus(
-//                     userOptionData.totalFee
-//                 );
-//                 WeeklyLeaderboardEntity.totalTrades = WeeklyLeaderboardEntity.totalTrades + 1;
-//                 WeeklyLeaderboardEntity.netPnL = WeeklyLeaderboardEntity.netPnL.minus(
-//                     userOptionData.totalFee
-//                 );
-//                 WeeklyLeaderboardEntity.winRate = (WeeklyLeaderboardEntity.tradesWon * 100000) / WeeklyLeaderboardEntity.totalTrades;
-//                 WeeklyLeaderboardEntity.save();
-//             }
-//         } else {
-//             throw console.error(
-//                 "User option data not found for id {} and contract {}",
-//                 [event.params.id.toString(), event.address.toHexString()]
-//             );
-//         }
-//     }
-// }
-
-// export function handleUpdateReferral(event: UpdateReferral): void {
-//     let routerContract = BufferRouter.bind(Address.fromString(RouterAddress));
-//     let optionContractInstance = BufferBinaryOptions.bind(
-//         event.address
-//     );
-//     if (routerContract.contractRegistry(event.address) == true) {
-//         if (optionContractInstance.tokenX() == Address.fromString(USDC_ADDRESS)) {
-//             let user = event.params.user;
-//             let referrer = event.params.referrer;
-
-//             let userReferralData = _loadOrCreateReferralData(user);
-//             userReferralData.totalDiscountAvailed = userReferralData.totalDiscountAvailed.plus(
-//                 event.params.rebate
-//             );
-//             userReferralData.totalTradingVolume = userReferralData.totalTradingVolume.plus(
-//                 event.params.totalFee
-//             );
-//             userReferralData.save();
-
-//             let referrerReferralData = _loadOrCreateReferralData(referrer);
-//             referrerReferralData.totalTradesReferred += 1;
-//             referrerReferralData.totalVolumeOfReferredTrades = referrerReferralData.totalVolumeOfReferredTrades.plus(
-//                 event.params.totalFee
-//             );
-//             referrerReferralData.totalRebateEarned = referrerReferralData.totalRebateEarned.plus(
-//                 event.params.referrerFee
-//             );
-//             referrerReferralData.save();
-
-//             let dayID = _getDayId(event.block.timestamp);
-//             let userRewardEntity = _loadOrCreateUserRewards(
-//                 dayID,
-//                 event.block.timestamp
-//             );
-//             userRewardEntity.referralDiscount = userRewardEntity.referralDiscount.plus(event.params.rebate);
-//             userRewardEntity.referralReward = userRewardEntity.referralReward.plus(event.params.referrerFee);
-//             userRewardEntity.nftDiscount = userRewardEntity.cumulativeReward.minus(event.params.rebate);
-//             userRewardEntity.save()
-//         }
-//     }
-// }
+import { BLPtxns } from "../generated/schema";
+import { _updateOrCreatePoolStatForProfitLoss } from "./initialize";
 
 export function handleProvide(event: Provide): void {
-  let poolContractInstance = BinaryPool.bind(event.address);
-  let rate = poolContractInstance
-    .totalTokenXBalance()
-    .times(BigInt.fromI64(100000000))
-    .div(poolContractInstance.totalSupply());
+  const poolAddress = event.address;
 
-  let poolStat = _loadOrCreatePoolStat(
-    _getDayId(event.block.timestamp),
-    "daily"
-  );
-  let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
-  poolStat.amount = usdcContractInstance.balanceOf(event.address);
+  const blp_pool_txn = new BLPtxns(event.transaction.hash.toHexString());
 
-  poolStat.timestamp = event.block.timestamp;
-  poolStat.rate = rate;
-  poolStat.save();
+  blp_pool_txn.amount = event.params.amount;
+  blp_pool_txn.user = event.params.account;
+  blp_pool_txn.timestamp = event.block.timestamp;
+  blp_pool_txn.type = "Provide";
+  blp_pool_txn.txHash = event.transaction.hash;
+  blp_pool_txn.units_minted_burned = event.params.writeAmount;
+  blp_pool_txn.poolAddress = poolAddress;
+  blp_pool_txn.blp_rate = event.params.amount.div(event.params.writeAmount);
+  blp_pool_txn.save();
 
-  let totalPoolStat = _loadOrCreatePoolStat("total", "total");
-  totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
-
-  totalPoolStat.timestamp = event.block.timestamp;
-  totalPoolStat.save();
+  // _updateOrCreatePoolStatForProvideWithdraw(
+  //   event.block.timestamp,
+  //   event.params.amount,
+  //   event.params.writeAmount,
+  //   true
+  // );
 }
 
 export function handleWithdraw(event: Withdraw): void {
-  let poolContractInstance = BinaryPool.bind(event.address);
-  let rate = poolContractInstance
-    .totalTokenXBalance()
-    .times(BigInt.fromI64(100000000))
-    .div(poolContractInstance.totalSupply());
+  const poolAddress = event.address;
 
-  let poolStat = _loadOrCreatePoolStat(
-    _getDayId(event.block.timestamp),
-    "daily"
-  );
-  let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
-  poolStat.amount = usdcContractInstance.balanceOf(event.address);
-  poolStat.timestamp = event.block.timestamp;
-  poolStat.rate = rate;
-  poolStat.save();
+  const blp_pool_txn = new BLPtxns(event.transaction.hash.toHexString());
 
-  let totalPoolStat = _loadOrCreatePoolStat("total", "total");
-  totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
+  blp_pool_txn.amount = event.params.amount;
+  blp_pool_txn.user = event.params.account;
+  blp_pool_txn.timestamp = event.block.timestamp;
+  blp_pool_txn.type = "Withdraw";
+  blp_pool_txn.txHash = event.transaction.hash;
+  blp_pool_txn.units_minted_burned = event.params.writeAmount;
+  blp_pool_txn.poolAddress = poolAddress;
+  blp_pool_txn.blp_rate = event.params.amount.div(event.params.writeAmount);
+  blp_pool_txn.save();
 
-  totalPoolStat.timestamp = event.block.timestamp;
-  totalPoolStat.save();
+  // _updateOrCreatePoolStatForProvideWithdraw(
+  //   event.block.timestamp,
+  //   event.params.amount,
+  //   event.params.writeAmount,
+  //   false
+  // );
 }
 
 export function handleProfit(event: Profit): void {
-  let poolContractInstance = BinaryPool.bind(event.address);
-  let rate = poolContractInstance
-    .totalTokenXBalance()
-    .times(BigInt.fromI64(100000000))
-    .div(poolContractInstance.totalSupply());
-  let poolStat = _loadOrCreatePoolStat(
-    _getDayId(event.block.timestamp),
-    "daily"
+  _updateOrCreatePoolStatForProfitLoss(
+    event.block.timestamp,
+    event.params.amount,
+    true
   );
-  let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
-  poolStat.amount = usdcContractInstance.balanceOf(event.address);
-
-  poolStat.timestamp = event.block.timestamp;
-  poolStat.rate = rate;
-  poolStat.save();
-
-  let totalPoolStat = _loadOrCreatePoolStat("total", "total");
-  totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
-
-  totalPoolStat.timestamp = event.block.timestamp;
-  totalPoolStat.save();
+  // let poolContractInstance = BinaryPool.bind(event.address);
+  // let rate = poolContractInstance
+  //   .totalTokenXBalance()
+  //   .times(BigInt.fromI64(100000000))
+  //   .div(poolContractInstance.totalSupply());
+  // let poolStat = _loadOrCreatePoolStat(
+  //   _getDayId(event.block.timestamp),
+  //   "daily"
+  // );
+  // let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
+  // poolStat.amount = usdcContractInstance.balanceOf(event.address);
+  // poolStat.timestamp = event.block.timestamp;
+  // poolStat.rate = rate;
+  // poolStat.save();
+  // let totalPoolStat = _loadOrCreatePoolStat("total", "total");
+  // totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
+  // totalPoolStat.timestamp = event.block.timestamp;
+  // totalPoolStat.save();
 }
 
 export function handleLoss(event: Loss): void {
-  let poolContractInstance = BinaryPool.bind(event.address);
-  let rate = poolContractInstance
-    .totalTokenXBalance()
-    .times(BigInt.fromI64(100000000))
-    .div(poolContractInstance.totalSupply());
-  let poolStat = _loadOrCreatePoolStat(
-    _getDayId(event.block.timestamp),
-    "daily"
+  _updateOrCreatePoolStatForProfitLoss(
+    event.block.timestamp,
+    event.params.amount,
+    false
   );
-  let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
-  poolStat.amount = usdcContractInstance.balanceOf(event.address);
-  poolStat.timestamp = event.block.timestamp;
-  poolStat.rate = rate;
-  poolStat.save();
-
-  let totalPoolStat = _loadOrCreatePoolStat("total", "total");
-  totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
-  totalPoolStat.timestamp = event.block.timestamp;
-  totalPoolStat.save();
+  // let poolContractInstance = BinaryPool.bind(event.address);
+  // let rate = poolContractInstance
+  //   .totalTokenXBalance()
+  //   .times(BigInt.fromI64(100000000))
+  //   .div(poolContractInstance.totalSupply());
+  // let poolStat = _loadOrCreatePoolStat(
+  //   _getDayId(event.block.timestamp),
+  //   "daily"
+  // );
+  // let usdcContractInstance = USDC.bind(Address.fromString(USDC_ADDRESS));
+  // poolStat.amount = usdcContractInstance.balanceOf(event.address);
+  // poolStat.timestamp = event.block.timestamp;
+  // poolStat.rate = rate;
+  // poolStat.save();
+  // let totalPoolStat = _loadOrCreatePoolStat("total", "total");
+  // totalPoolStat.amount = usdcContractInstance.balanceOf(event.address);
+  // totalPoolStat.timestamp = event.block.timestamp;
+  // totalPoolStat.save();
 }
-
-// export function handlePause(event: Pause): void {
-//     let isPaused = event.params.isPaused;
-//     let optionContract = _loadOrCreateOptionContractEntity(event.address);
-//     optionContract.isPaused = isPaused;
-//     optionContract.save()
-// }

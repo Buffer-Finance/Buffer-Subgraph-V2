781e4fd3cea28fb68a7f90c28bb7ce7440b32350
Faraday <faraday@buffer.finance>
2024-03-20 12:24:46 +0530

    add jackpot

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
+import { JackpotTriggered } from "../generated/Jackpot/Jackpot";
 import { RebateClaimed } from "../generated/TraderRebate/TraderRebate";
-import { Rebate } from "../generated/schema";
+import { JackpotData, Rebate } from "../generated/schema";
 
 export function handleRebateClaimed(event: RebateClaimed): void {
   const rebateEntity = new Rebate(
@@ -12,4 +13,18 @@ export function handleRebateClaimed(event: RebateClaimed): void {
   rebateEntity.timestamp = event.block.timestamp;
   rebateEntity.type = "Rebates";
   rebateEntity.save();
+}
+
+export function handleJackpotTriggered(event: JackpotTriggered): void {
+  const jackpotEntity = new JackpotData(event.transaction.hash.toHexString());
+
+  jackpotEntity.optionContract = event.params.optionContract;
+  jackpotEntity.optionID = event.params.optionId;
+  jackpotEntity.user = event.params.userAddress;
+  jackpotEntity.timestamp = event.block.timestamp;
+  jackpotEntity.amount = event.params.amount;
+  jackpotEntity.routerContract = event.params.router;
+  jackpotEntity.jackpotAmount = event.params.jackpotWinAmount;
+  jackpotEntity.txn_hash = event.transaction.hash;
+  jackpotEntity.save();
 }
